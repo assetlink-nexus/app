c1367c472d966154d7cb3984af5745675cba50c0
Dominik <d1n22@onet.pl>
2024-02-18 12:59:56 +0100

    ADD router sign in

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BuyDashboardComponent } from './views/buy-dashboard/buy-dashboard.component';
+import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   { path: '', component: BuyDashboardComponent },
-  //   { path: 'login', component: RecipeDetailComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
