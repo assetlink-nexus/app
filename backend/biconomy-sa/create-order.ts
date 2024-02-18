f116ee6d63e7e08b3a7738646b9a1bd757eab7fe
Yazep <andreiyazepchyk@gmail.com>
2024-02-18 11:39:28 +0100

    ADD chain dodalem compile i deploy smart kontraktow

--- backend/biconomy-sa/create-order.ts
+++ backend/biconomy-sa/create-order.ts
@@ -1,30 +1,26 @@
 import {
-    Hex,
-    createWalletClient,
-    encodeFunctionData,
-    http,
-    parseAbi,
-    zeroAddress,
-  } from "viem";
-  import { privateKeyToAccount } from "viem/accounts";
-  import { polygonMumbai } from "viem/chains";
-  import { createSmartAccountClient } from "@biconomy/account";
-  
-  const bundlerUrl =
-    "https://bundler.biconomy.io/api/v2/80001/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44"; // Found at https://dashboard.biconomy.io
-  
-  export const createAccountAndMintNft = async () => {
-    // ----- 1. Generate EOA from private key
-    const privateKey = "b3d192521ddc8b77cd442df971a944e193322f8580f72238d07afbbedf0973fb";
-    const account = privateKeyToAccount(`0x${privateKey}`);
-    const client = createWalletClient({
-      account,
-      chain: polygonMumbai,
-      transport: http(),
-    });
-    const eoa = client.account.address;
-    console.log(`EOA address: ${eoa}`);
-  
+  Hex,
+  createWalletClient,
+  encodeFunctionData,
+  http,
+  parseAbi,
+  zeroAddress,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { polygonMumbai } from "viem/chains";
+import { createSmartAccountClient } from "@biconomy/account";
+
+export const createAccountAndMintNft = async () => {
+  // ----- 1. Generate EOA from private key
+  const account = privateKeyToAccount(`0x${"b3d192521ddc8b77cd442df971a944e193322f8580f72238d07afbbedf0973fb"}`);
+  const client = createWalletClient({
+    account,
+    chain: polygonMumbai,
+    transport: http(),
+  });
+  const eoa = client.account.address;
+  console.log(`EOA address: ${eoa}`);
+
   // ------ 2. Create biconomy smart account instance
   const smartAccount = await createSmartAccountClient({
     signer: client,
